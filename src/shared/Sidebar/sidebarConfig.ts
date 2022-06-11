f3f403be5324c6828c649055b37aebf07a83ba2d
Varentsov Andrey <varentsov.andrey@gmail.com>
2022-06-11 08:44:37 +0300

    6 lesson

---
+++ src/shared/Sidebar/sidebarConfig.ts
@@ -0,0 +1,12 @@
+export const sidebarConfig = [
+  {
+    id: 1,
+    name: 'Страница пользователей',
+    path: '/users_page',
+  },
+  {
+    id: 2,
+    name: 'Тестовая страница',
+    path: '/test_page',
+  },
+];
